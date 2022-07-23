65eee8e04903396714e7a4a9228c27dd7db222ea
wliang <wliang@infervision.com>
2022-07-23 20:22:09 +0800

    feat: vscode prettierrc setting

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  endOfLine: 'auto',
+  tabWidth: 2,
+  quoteProps: 'as-needed',
+  printWidth: 400,
+  semi: true,
+  useTabs: false,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
