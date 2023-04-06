44246550b07e18375776a83eaa3831e61a9cf83c
wliang <wliang@infervision.com>
2023-04-06 23:42:01 +0800

    chore: 重写calculateTextSegments函数

--- lib/calculateTextSegments.ts
+++ lib/calculateTextSegments.ts
@@ -1,75 +1,88 @@
-function calculateTextSegments(text, remainingHeight, maxHeight, width, font) {
-  // 创建一个测试文字宽高的元素, 并把他插入body中
-  const span = document.createElement('span');
-  span.style.fontSize = font.fontSize;
-  span.style.visibility = 'hidden';
-  document.body.appendChild(span);
+function calculateTextSegments(textList, remainingHeight, maxHeight, width, textStyle) {
+  // 获取测量函数及卸载测量元素方法
+  const { measureFontTextFn, destoryMeasureNode } = measureFontTextWH(textStyle);
+  const result = [];
 
-  const lines = text.split('\n');
-  const segments = [];
-  let currentSegment = '';
-  let currentHeight = 0;
+  const getCurDivH = index => {
+    if (index === 0) {
+      return remainingHeight;
+    }
+    return maxHeight;
+  };
 
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    const { width: lineWidth, height: lineHeight } = calculateLineHeightAndWidth(span, line, width); // 计算行宽度
-
-    if (currentHeight + lineHeight > maxHeight) {
-      segments.push(currentSegment.trim());
-      currentSegment = line;
-      currentHeight = lineHeight;
-    } else if (currentHeight + lineHeight <= remainingHeight) {
-      currentSegment += '\n' + line;
-      currentHeight += lineHeight;
-    } else if (lineWidth <= width) {
-      segments.push(currentSegment.trim());
-      currentSegment = line;
-      currentHeight = lineHeight;
-    } else {
-      const words = line.split(' ');
-      let newLine = '';
-      for (let j = 0; j < words.length; j++) {
-        const testLine = newLine === '' ? words[j] : newLine + ' ' + words[j];
-        const testWidth = calculateLineHeightAndWidth(span, testLine, width);
-        if (testWidth > width && newLine !== '') {
-          segments.push(newLine);
-          newLine = words[j];
-        } else {
-          newLine = testLine;
-        }
+  let divIndex = 0;
+  //
+  let curIndex = 0;
+  for (let i = 0; i < textList.length; i++) {
+    const seg = textList[i];
+    for (let j = 0; j < seg.length; j++) {
+      if (curIndex != i) {
+        curIndex = i;
       }
-      currentSegment += '\n' + newLine;
-      currentHeight += lineHeight;
+      let curLine = '';
+      let curLineH = 0;
+      let curLineW = 0;
+      let curContentH = 0;
+      const { w, h } = measureFontTextFn(seg[j]);
+      if (curLineW <= width) {
+        curLine += seg[j];
+        curLineH = Math.max(h, curLineH);
+      } else {
+        let curDiv = result[divIndex];
+        if (Array.isArray(curDiv['content'])) {
+          const curDivH = getCurDivH(divIndex);
+          if (curDiv.height + curLine <= curDivH) {
+            curDiv.content.push(curLine);
+            curDiv.height += curLineH;
+          } else {
+            divIndex += 1;
+            result[divIndex] = { content: [], height: 0 };
+            curDiv = result[divIndex];
+            curDiv.content.push(curLine);
+            curDiv.height += curLineH;
+          }
+        } else {
+          result[divIndex] = { content: [], height: 0 };
+        }
+        curLine = '';
+        curLineH = 0;
+        curLineW = 0;
+      }
     }
   }
-
-  segments.push(currentSegment.trim());
-
-  // 移除测试文字宽高的元素
-  document.body.removeChild(span);
-
-  return segments;
+  destoryMeasureNode();
+  return result;
 }
 
-// function calculateLineHeightAndWidth(line, width) {
-//   // 计算单行文字的宽度
-//   const canvas = document.createElement('canvas');
-//   const context = canvas.getContext('2d');
-//   context.font = '16px Arial'; // 假设字号为16px
-//   return context.measureText(line).width;
-// }
+export function measureFontTextWH(textStyle) {
+  let fontFamily = textStyle.fontFamily ? textStyle.fontFamily : '';
+  let fontSize = textStyle.fontSize ? textStyle.fontSize + 'px' : '';
+  let fontWeight = textStyle.fontWeight ? textStyle.fontWeight : '';
 
-function calculateLineHeightAndWidth(node, text, font) {
-  // span.style.opacity = 0;
-  node.textContent = text;
-  const { width, height } = node.getBoundingClientRect();
-  return { width, height };
+  let measureNode = document.createElement('div');
+  measureNode.style.fontFamily = fontFamily;
+  measureNode.style.fontSize = fontSize;
+  measureNode.style.fontWeight = fontWeight;
+
+  measureNode.style.visibility = 'hidden';
+  measureNode.style.display = 'inline-block';
+  measureNode.style.whiteSpace = 'nowrap';
+
+  document.body.appendChild(measureNode);
+
+  const measureFontTextFn = text => {
+    measureNode.textContent = text;
+    let w = measureNode.offsetWidth;
+    let h = measureNode.offsetHeight;
+    return { w: w, h: h };
+  };
+
+  const destoryMeasureNode = () => {
+    document.body.removeChild(measureNode);
+    return true;
+  };
+
+  return { measureFontTextFn, destoryMeasureNode };
 }
 
-// function measureTextWidth(text, font) {
-//   const offscreenCanvas = new OffscreenCanvas(0, 0);
-//   const context = offscreenCanvas.getContext('2d');
-//   context.font = font;
-//   const width = context.measureText(text).width;
-//   return width;
-// }
+export default calculateTextSegments;
--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,6 @@ import sqrt from './sqrt';
 import toFixed from './toFixed';
 import isType from './isType';
 import pLimit from './pLimit';
+import calculateTextSegments from './calculateTextSegments';
 
-export { sqrt, toFixed, isType, pLimit };
+export { sqrt, toFixed, isType, pLimit, calculateTextSegments };
